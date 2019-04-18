878538cbc7c6d6f2c8445b16080612732683f967
edencaox <edencaox@gmail.com>
2019-04-18 13:46:50 +0800

    update

---
+++ basic_data_type.py
@@ -0,0 +1,15 @@
+#coding=utf-8
+
+counter = 100
+miles   = 1000
+name  = "runoob"
+
+print(counter)
+print(miles)
+print(name)
+
+print("===============")
+
+a, b, c, d = 20, 5.5, True, 4 + 3j
+print(type(a), type(b), type(c), type(d))
+print(isinstance(a, int))
---
+++ str.py
@@ -0,0 +1,15 @@
+#coding=utf-8
+
+str = "Runoob"
+
+print(str)
+print(str[0: -1])
+print(str[0])
+print(str[2:5])
+print(str[2:])
+print(str * 2)
+print(str + "TEST")
+
+escape_str = "Run\noob"
+print(escape_str)
+print(r"Run\noob")
