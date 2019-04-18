14025ee449d5b540c708dcf634af83ef73398cbe
edencaox <edencaox@gmail.com>
2019-04-18 17:56:00 +0800

    add func

---
+++ fun.py
@@ -0,0 +1,15 @@
+def hello():
+  print("Hello world")
+
+hello()
+
+def area(width, height):
+  return width * height
+
+def print_welcome(name):
+  print("Welcome", name)
+
+print_welcome("Runoob")
+w = 4
+h = 5
+print("width = ", w, "; height = ", h, "; area = ", area(w, h), ";")
